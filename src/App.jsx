9dc84c2d8dff2126e0546ce930f0e6cffb302cbb
somramnani <som.ramnani@gmail.com>
2023-11-21 22:01:13 -0500

    add tailwind, postcss & import css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import "./index.css";
+
 const App = () => {
   return (
     <div>
       {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
   );
 };
